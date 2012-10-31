8663f6a33ce6310e2ef3db3c1b957fd83bdf1deb
Unscrewed <nathanfrissen@live.nl>
2012-10-31 01:37:10 +0100

    Updated iBlock files

--- src/me/Unscrewed/iBlock/iBlock.java
+++ src/me/Unscrewed/iBlock/iBlock.java
@@ -1,7 +1,7 @@
 package me.Unscrewed.iBlock;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -27,12 +27,16 @@ public class iBlock extends JavaPlugin {
 		blockListener = new iBlockListener(this);
 		playerListener = new iPlayerListener(this);
 		entityListener = new iEntityListener(this);
+		
 		PluginDescriptionFile pdfFile = this.getDescription();
 		log.info("[iBlock] Version " + pdfFile.getVersion() + " enabled successfully!");
 		@SuppressWarnings("unused")
 		PluginManager pm = getServer().getPluginManager();
-		loadConfiguration();
-		deniedBlocksPlace = getConfig().getIntegerList("bannedBlocksPlace");
+		File configFile = new File(getDataFolder(), "config.yml");
+        if ( !configFile.exists())
+        saveDefaultConfig();
+        reloadConfig();
+        deniedBlocksPlace = getConfig().getIntegerList("bannedBlocksPlace");
 		deniedBlocksBreak = getConfig().getIntegerList("bannedBlocksBreak");
 		@SuppressWarnings("unused")
 		Updater updater = new Updater(this, "iBlock", this.getFile(), Updater.UpdateType.DEFAULT, false);
@@ -44,27 +48,6 @@ public class iBlock extends JavaPlugin {
 	}
 
 	public void loadConfiguration() {
-		String bannedBucketWaterM = "waterBucketM";
-		String bannedBucketLavaM = "lavaBucketM";
-		String bannedBlocksPlace = "bannedBlocksPlace";
-		String bannedBlocksBreak = "bannedBlocksBreak";
-		String bannedBlocksPlaceM = "bannedBlocksPlaceM";
-		String bannedBlocksBreakM = "bannedBlocksBreakM";
-		String warnAdmins = "warnAdmins";
-		String bannedBucketWaterMV = "You are not allowed to place water here.";
-		String bannedBucketLavaMV = "You are not allowed to place lava here.";
-		String bannedBlocksPlaceMV = "You are not allowed to place this block.";
-		String bannedBlocksBreakMV = "You are not allowed to break this block.";
-		String[] bannedBlocksPlaceList = { "7", "9", "11", "19", "30", "52", "78", "79", "97", "120" };
-		String[] bannedBlocksBreakList = { "7" };
-		String warnAdminsT = "true";
-		this.getConfig().addDefault(warnAdmins, warnAdminsT);
-		this.getConfig().addDefault(bannedBucketWaterM, bannedBucketWaterMV);
-		this.getConfig().addDefault(bannedBucketLavaM, bannedBucketLavaMV);
-		this.getConfig().addDefault(bannedBlocksPlaceM, bannedBlocksPlaceMV);
-		this.getConfig().addDefault(bannedBlocksBreakM, bannedBlocksBreakMV);
-		this.getConfig().addDefault(bannedBlocksPlace, Arrays.asList(bannedBlocksPlaceList));
-		this.getConfig().addDefault(bannedBlocksBreak, Arrays.asList(bannedBlocksBreakList));
 		this.getConfig().options().copyDefaults(true);
 		this.saveConfig();
 	}
